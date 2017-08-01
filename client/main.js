84e999042a5787fc0e437c79346ef84dabec54ef
sophieklm <sklm@live.co.uk>
2017-08-01 12:19:14 +0100

    adds app component and refactors

--- client/main.js
+++ client/main.js
@@ -3,21 +3,12 @@ import ReactDOM from 'react-dom';
 import {Meteor} from 'meteor/meteor';
 import {Tracker} from 'meteor/tracker';
 import {Players} from './../imports/api/players';
-import TitleBar from './../imports/ui/TitleBar';
-import AddPlayer from './../imports/ui/AddPlayer';
-import PlayerList from './../imports/ui/PlayerList';
+import App from './../imports/ui/App';
 
 Meteor.startup(() => {
   Tracker.autorun(() =>{
     let players = Players.find().fetch();
     let title = "Score Keep";
-    let jsx = (
-      <div>
-        <TitleBar title={title} subtitle="Created by Sophie Mann"/>
-        <PlayerList players={players}/>
-        <AddPlayer/>
-      </div>
-    );
-    ReactDOM.render(jsx, document.getElementById('app'));
+    ReactDOM.render(<App title={title} players={players}/>, document.getElementById('app'));
   });
 });
--- imports/ui/TitleBar.js
+++ imports/ui/TitleBar.js
@@ -2,11 +2,16 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 export default class TitleBar extends React.Component {
+  renderSubtitle() {
+    if (this.props.subtitle) {
+      <h2>{this.props.subtitle}</h2>;
+    }
+  }
   render() {
     return (
       <div>
         <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
+        {this.renderSubtitle()}
       </div>
     );
   }
