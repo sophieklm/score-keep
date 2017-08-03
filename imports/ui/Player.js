b0f050b66ba5d42a3225499f52086717780ee6ec
sophieklm <sklm@live.co.uk>
2017-08-03 20:19:37 +0100

    adds ranking styles

--- imports/ui/Player.js
+++ imports/ui/Player.js
@@ -4,15 +4,14 @@ import {Players} from './../api/players';
 
 export default class Player extends React.Component {
   render() {
+    let itemClassName= `item item--position-${this.props.player.rank}`;
     return (
-      <div className="item" key={this.props.player._id}>
+      <div className={itemClassName} key={this.props.player._id}>
         <div className="player">
           <div>
             <h3 className="player__name">{this.props.player.name}</h3>
             <p className="player__stats">
-              {this.props.player.rank}
-              {this.props.player.position}
-              {this.props.player.score} point(s)
+              {this.props.player.position} {this.props.player.score} point(s)
             </p>
           </div>
           <div className="player__actions">
