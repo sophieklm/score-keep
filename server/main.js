f18c28ce8d16f850ce181118608677d4f34e5b2e
sophieklm <sklm@live.co.uk>
2017-08-03 20:31:10 +0100

    updates server

--- server/main.js
+++ server/main.js
@@ -2,11 +2,4 @@ import {Meteor} from 'meteor/meteor';
 import {Players} from './../imports/api/players';
 
 Meteor.startup(function () {
-  let obj = {
-    name: "Sophie",
-    printName() {
-      console.log(`Name: ${this.name}`);
-    }
-  };
-  setTimeout(obj.printName.bind(obj), 1000);
 });
